5fcdb76d2f06f004a70e68292fdd834636679ff6
i-kovalyshyn <ikovalyshyn@gmail.com>
2019-08-07 11:02:56 +0300

    was changed to a boolean condition

--- src/main/java/FireDepartment/Alarm.java
+++ src/main/java/FireDepartment/Alarm.java
@@ -1,23 +1,23 @@
 package main.java.FireDepartment;
 
 class Alarm {
-    private int ring = 0;
+    private boolean ring;
 
     synchronized void alarmOff() throws InterruptedException {
-        while (ring < 1) {
+        while (!ring) {
             wait();
         }
-        ring--;
+        ring = false;
         System.out.println("Everything good - on -" + Thread.currentThread());
         System.out.println("Fireman wait for alarm");
         notify();
     }
 
     synchronized void alarmOn() throws InterruptedException {
-        while (ring >= 1) {
+        while (ring) {
             wait();
         }
-        ring++;
+        ring = true;
 
         System.out.println("ALARM, FIRE - on -" + Thread.currentThread());
         System.out.println("The fire was extinguished");
